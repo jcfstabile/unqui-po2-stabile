3a5e7739a0256d6948a9e88c7f22c97442b85b5e
Juan Carlos Francisco Stabile <jcfstabile@gmail.com>
2020-04-22 15:45:31 -0300

    add rectangulo a ej7 test

--- test/ar/edu/unq/po2/tp3/Ej7Tests.java
+++ test/ar/edu/unq/po2/tp3/Ej7Tests.java
@@ -11,34 +11,38 @@ import org.junit.jupiter.api.DisplayName;
 
 class Ej7Tests {
 
-    Rectangulo rectangulo;
-    Rectangulo rectanguloVertical;
+    Rectangulo rectangulo2x5;
+    Rectangulo rectangulo7x3;
 
     @BeforeEach
     void setUp(){
-        rectanguloVertical = new Rectangulo(0.0f, 0.0f, 5.0f, 2.0f);
-        rectangulo = new Rectangulo(0.0f, 0.0f, 2.0f, 5.0f);
+        rectangulo2x5 = new Rectangulo(0.0f, 0.0f, 2.0f, 5.0f);
+        rectangulo7x3 = new Rectangulo(1.0f, 2.0f, 7.0f, 3.0f);
     }
 
-    @DisplayName(" Hay un Rectangulo con area 10")
+    @DisplayName(" Hay un Rectangulo con area 10 y otro de area 21")
     @Test
     void testRectanguloArea(){
-        assertEquals(10, rectangulo.area());
+        assertEquals(10, rectangulo2x5.area());
+        assertEquals(21, rectangulo7x3.area());
     }
-    @DisplayName(" Hay un rectangulo con perimetro 14")
+    @DisplayName(" Hay un rectangulo con perimetro 14 y otro de 20")
     @Test
     void testRectanguloPerimetro(){
-        assertEquals(14, rectangulo.perimetro());
+        assertEquals(14, rectangulo2x5.perimetro());
+        assertEquals(20, rectangulo7x3.perimetro());
     }
-    @DisplayName(" Hay un rectangulo horizontal")
+    @DisplayName(" Hay un rectangulo horizontal y otro no")
     @Test
     void testRectanguloHorizontal(){
-        assertTrue(rectangulo.esHorizontal());
+        assertTrue(rectangulo2x5.esHorizontal());
+        assertFalse(rectangulo7x3.esHorizontal());
     }
-    @DisplayName(" Hay un rectangulo vertical")
+    @DisplayName(" Hay un rectangulo vertical y otro no")
     @Test
     void testRectanguloVertical(){
-        assertTrue(rectanguloVertical.esVertical());
+        assertFalse(rectangulo2x5.esVertical());
+        assertTrue(rectangulo7x3.esVertical());
     }
     @DisplayName(" Hay un cuadrado de area 25 perimerto 20 y que no es rectangulo ni horizontal ni vertical")
     @Test
